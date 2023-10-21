8ce41ba03cde89039850eb8f33af75934ad4310c
e41c <ericgrigor@gmail.com>
2023-10-20 23:35:17 -0400

    user-service running successfully, "Completed initialization in 2 ms"
    running with jpa and postgres , docker

--- mircoservice-parent/user-service/src/main/java/ca/gbc/userservice/service/UserServiceImpl.java
+++ mircoservice-parent/user-service/src/main/java/ca/gbc/userservice/service/UserServiceImpl.java
@@ -3,10 +3,13 @@ package ca.gbc.userservice.service;
 import ca.gbc.userservice.dto.UserRequest;
 import ca.gbc.userservice.dto.UserResponse;
 import ca.gbc.userservice.model.User;
+import ca.gbc.userservice.model.UserLineItem;
 import ca.gbc.userservice.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.postgresql.core.Query;
+import org.springframework.data.jpa.repository.support.QuerydslJpaRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,6 +19,7 @@ import java.util.List;
 @Transactional
 @Slf4j
 public class UserServiceImpl implements UserService {
+    private final QuerydslJpaRepository querydslJpaRepository;
     private final UserRepository userRepository;
 
     @Override
@@ -33,16 +37,22 @@ public class UserServiceImpl implements UserService {
         return null;
     }
 
-
     @Override
     public void deleteUser(String userId) {
-
-
+        userRepository.deleteById(Long.valueOf(userId));
     }
 
     @Override
     public List<UserResponse> getAllUsers() {
-        return null;
+        List<User> user = userRepository.findAll();
+        return user.stream().map(this::mapToDto).toList();
+    }
+
+    private UserLineItem mapToDto(UserLineItem userLineItem){
+        UserLineItem userLineItem1 = new UserLineItem();
+        userLineItem1.setUsername(userLineItem.getUsername());
+        userLineItem1.setEmail(userLineItem.getEmail());
+        return userLineItem1;
     }
 
 }
