88ea6375112bb276244be097dca1570cb34150d8
christriwu <60164298+christriwu@users.noreply.github.com>
2023-12-12 22:54:22 -0500

    eureka server and api-gateway

--- mircoservice-parent/api-gateway/src/main/java/ca/gbc/apigateway/ApiGatewayApplication.java
+++ mircoservice-parent/api-gateway/src/main/java/ca/gbc/apigateway/ApiGatewayApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableDiscoveryClient // This annotation is required for the API Gateway to be able to discover the services it needs to route requests to.
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
