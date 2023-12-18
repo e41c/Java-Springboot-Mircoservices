b3e0e22805432f7b3d28d0f4f82e1d19b60628fe
christriwu <60164298+christriwu@users.noreply.github.com>
2023-12-17 19:58:44 -0500

    eureka server and api-gateway

--- mircoservice-parent/api-gateway/src/main/java/ca/gbc/apigateway/SecurityConfig/SecurityConfig.java
+++ mircoservice-parent/api-gateway/src/main/java/ca/gbc/apigateway/SecurityConfig/SecurityConfig.java
@@ -1,36 +1,36 @@
-package ca.gbc.apigateway.SecurityConfig;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
-import org.springframework.security.oauth2.jwt.ReactiveJwtDecoders;
-import org.springframework.security.web.server.SecurityWebFilterChain;
-
-@Configuration
-@EnableWebFluxSecurity
-public class SecurityConfig {
-
-    @Value("${spring.security.oauth2.resourceserver.issuer-uri}")
-    private String issuerUri;
-
-    @Bean
-    public ReactiveJwtDecoder jwtDecoder() {
-        return ReactiveJwtDecoders.fromIssuerLocation(issuerUri);
-    }
-
-    @Bean
-    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity){
-        serverHttpSecurity
-                .csrf(csrf -> csrf.disable())
-                .authorizeExchange(exchange -> exchange.pathMatchers("/eureka/**")
-                        .permitAll()
-                        .anyExchange()
-                        .authenticated())
-                .oauth2ResourceServer(configurer -> configurer.jwt(jwt -> jwt.jwtDecoder(jwtDecoder())));
-
-        return serverHttpSecurity.build();
-    }
-}
+//package ca.gbc.apigateway.SecurityConfig;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+//import org.springframework.security.config.web.server.ServerHttpSecurity;
+//import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
+//import org.springframework.security.oauth2.jwt.ReactiveJwtDecoders;
+//import org.springframework.security.web.server.SecurityWebFilterChain;
+//
+//@Configuration
+//@EnableWebFluxSecurity
+//public class SecurityConfig {
+//
+//    @Value("${spring.security.oauth2.resourceserver.issuer-uri}")
+//    private String issuerUri;
+//
+//    @Bean
+//    public ReactiveJwtDecoder jwtDecoder() {
+//        return ReactiveJwtDecoders.fromIssuerLocation(issuerUri);
+//    }
+//
+//    @Bean
+//    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity){
+//        serverHttpSecurity
+//                .csrf(csrf -> csrf.disable())
+//                .authorizeExchange(exchange -> exchange.pathMatchers("/eureka/**")
+//                        .permitAll()
+//                        .anyExchange()
+//                        .authenticated())
+//                .oauth2ResourceServer(configurer -> configurer.jwt(jwt -> jwt.jwtDecoder(jwtDecoder())));
+//
+//        return serverHttpSecurity.build();
+//    }
+//}
